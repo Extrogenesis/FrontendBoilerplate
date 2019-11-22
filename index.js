4a6c96ffdcd7acbe698ba9a516c6499b8fbab5c8
extrogenesis <extrogenesis@gmail.com>
2019-11-22 16:12:16 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let root = document.body
+
+let div = document.createElement('div')
+let text = document.createTextNode('Hello from index.js')
+
+div.appendChild(text)
+root.appendChild(div)
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    mode: 'development',
+    entry: './index.js',
+    output: {
+        filename: 'main.js',
+        publicPath: 'dist'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
+    }
+}
